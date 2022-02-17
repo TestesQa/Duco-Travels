a74b26646cba8a89d10436dc32b50ee763461852
unknown <williancruz@alunos.utfpr.edu.br>
2022-02-17 09:34:21 -0300

    Função validar repetidos excel

---
+++ validador.py
@@ -0,0 +1,56 @@
+from pickle import APPEND
+from openpyxl import Workbook, load_workbook
+# If you need to get the column letter, also import this
+from openpyxl.utils import get_column_letter
+from collections import Counter
+
+#Carrega planilha completa
+wb = load_workbook('agendas/Agenda.xlsx')
+
+#Lista as abas dentro da planilha
+for sheet in wb:
+    print(sheet)
+
+#Seleciona Aba a ser utilizada
+ws = wb['teste.xlsx']
+
+#Cria lista 
+agenda = []
+   
+#Preenche a lista com os dados necessário, informar no rangue onde começa e onde termina e informar qual coluna vai extrair os dados.
+for n in range(3,10653):
+
+    col = str(n)
+    my_value = ws['E'+ col].value
+    if my_value != 'Break' or my_value != None:
+        agenda.append(my_value)  
+        n = int(n)
+    else:
+        pass
+    
+
+#Criar contador
+contador = Counter(agenda)
+
+# Cria lista de repetidos
+
+repetidos = [
+        item for item, quantidade in contador.items() 
+            if quantidade > 1
+    ]
+
+#Apresenta valor de repeditos
+quantidade_repetidos = len(repetidos)-2
+#repetidos.pop('Break')
+# repetidos.pop()
+# repetidos.pop()
+
+
+
+#Printa lista de repetido e quantidade
+print(repetidos, quantidade_repetidos)
+    
+
+
+
+
